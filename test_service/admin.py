ab870028dc6952b1f704eab92632d7c6e468441c
Polina Koval <zagadka1316@gmail.com>
2022-12-01 17:37:38 +0300

    Add models:Question, Answer, Test.

--- test_service/admin.py
+++ test_service/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from test_service.models import Answer, Question, Test
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+
+class AnswerInLine(admin.TabularInline):
+    model = Answer
+    extra = 0
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["title"]
+    inlines = (AnswerInLine,)
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    pass
--- test_service/models.py
+++ test_service/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    title = models.TextField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_right = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Test(models.Model):
+    title = models.TextField()
+    question = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return self.title
