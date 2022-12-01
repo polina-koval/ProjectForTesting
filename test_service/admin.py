77baaa4ee6c632ff3db1f9546dd15d0a5c8687a5
Polina Koval <zagadka1316@gmail.com>
2022-12-01 20:08:47 +0300

    Add to list_display nested fields.

--- test_service/admin.py
+++ test_service/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from test_service.models import Answer, Question, Test
 
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("title", "is_right")
 
 
 class AnswerInLine(admin.TabularInline):
@@ -15,18 +16,32 @@ class AnswerInLine(admin.TabularInline):
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ["title"]
+    list_display = ("title", "get_answers")
     inlines = (AnswerInLine,)
 
     def save_model(self, request, obj, form, change):
-        answers_count = obj.answers.filter(is_right=True).count()
-        print(answers_count)
-        if answers_count > 0:
-            super().save_model(request, obj, form, change)
-        else:
+        answers = obj.answers.all()
+        true_answers = answers.filter(is_right=True).count()
+        false_answers = answers.filter(is_right=True).count()
+        if not true_answers:
             raise ValueError("At least one answer must be correct.")
+        if not false_answers:
+            raise ValueError("At least one answer must be wrong.")
+        super().save_model(request, obj, form, change)
+
+    def get_answers(self, obj: Question):
+        output = [answer.title for answer in obj.answers.all()]
+        return format_html("<br />".join(output))
+
+    get_answers.short_description = "Answers"
 
 
 @admin.register(Test)
 class TestAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("title", "get_questions")
+
+    def get_questions(self, obj: Test):
+        output = [question.title for question in obj.questions.all()]
+        return format_html("<br />".join(output))
+
+    get_questions.short_description = "Questions"
